19f75e9cf7c994f20ace0dd30dc4f0f9bdf2187a
betty calpas <betty.calpas@cern.ch>
2016-04-25 13:31:24 +0300

    Code used for publication.

--- test/batchJobs/submit/run_addMassVariables.sh
+++ test/batchJobs/submit/run_addMassVariables.sh
@@ -12,41 +12,31 @@ path=(/home/calpas/svfitMEM/CMSSW_8_0_0/src/TauAnalysis/SVfitPerformanceStudies/
 pathToOutput=(${path}batchJobs/output)
 pathToConfigFile=(${path}cfgTemplate)
 
-#samples=(GluGluHToTauTauM125)
-
-samples=(SUSYGluGluToHToTauTauM200
-         SUSYGluGluToHToTauTauM300
-         SUSYGluGluToHToTauTauM500
-         SUSYGluGluToHToTauTauM800
-         SUSYGluGluToHToTauTauM1200
-         SUSYGluGluToHToTauTauM1800
-         SUSYGluGluToHToTauTauM2600
-         GluGluHToTauTauM125
+samples=(GluGluHToTauTauM125
          DYJetsToLLM50
-        ) 
+        )
 
-#rm -rf $pathToOutput
-mkdir -p $pathToOutput/$dir
+#samples=(SUSYGluGluToHToTauTauM200
+#         SUSYGluGluToHToTauTauM300
+#         SUSYGluGluToHToTauTauM500
+#         SUSYGluGluToHToTauTauM800
+#         SUSYGluGluToHToTauTauM1200
+#         SUSYGluGluToHToTauTauM1800
+#         SUSYGluGluToHToTauTauM2600
+#         GluGluHToTauTauM125
+#         DYJetsToLLM50
+#        ) 
+
 
 # run addMassVariables for each config file in an array batch style
 for dir in ${directories[*]}; do
-  rm -rf $pathToOutput/$dir
-  mkdir -p $pathToOutput/$dir
-
   for opt in ${genOptions[*]}; do
-    mkdir -p $pathToOutput/$dir/$opt
-
     for sample in ${samples[*]}; do
       workDir=$pathToOutput/$dir/$opt/$sample
+      rm -rf $workDir
       mkdir -p $workDir 
       script="scriptTemplate/addMassVariables_${dir}_${opt}_${sample}"
-
-      if [ $sample = 'DYJetsToLLM50' ]; then
-        sbatch --array=0-1000 --workdir=$workDir --job-name=${dir}_${opt}_${sample} ${script}.sh
-      else
-        sbatch --array=0-500 --workdir=$workDir --job-name=${dir}_${opt}_${sample} ${script}.sh
-      fi
-
+      sbatch --array=0-1000 --workdir=$workDir --job-name=${dir}_${opt}_${sample} ${script}.sh
     done
   done
 done
